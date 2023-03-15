5719a87e63eea8b22ab9eab747e409d08e7fd4ac
RobinB87 <robinbette@hotmail.com>
2023-03-15 15:31:41 +0000

    add some styling fix the header a bit

--- frontend/src/app/shared/material/material.module.ts
+++ frontend/src/app/shared/material/material.module.ts
@@ -1,11 +1,21 @@
 
 import { NgModule } from '@angular/core';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule }from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule }from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   exports: [
-    MatButtonModule
+    MatSidenavModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
